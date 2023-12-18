f9689ce3d509fcae22c4efad24fdd8f23a5e5c11
jediros <47332765+jediros@users.noreply.github.com>
2023-12-17 21:28:35 -0500

    Update 02Image_resizing.py

--- 02Image_resizing.py
+++ 02Image_resizing.py
@@ -21,6 +21,8 @@ def install_modules():
 install_modules()
 ###############################################################################
 print("""
+###############################################################################
+
 IMAGE REZISING PROGRAM v1.0
 
 License: Open license
@@ -31,7 +33,10 @@ https://www.researchgate.net/profile/Jedi-Rosero-Alvarado-2
 IMPORTANT: This program was created to be run with specific .png or .jpg files.
 
 For more information or program changes, please contact the author.
+
+################################################################################
 """)
+
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import simpledialog
